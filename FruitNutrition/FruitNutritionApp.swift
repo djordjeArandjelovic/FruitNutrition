77d4b22c3d169a2084330768c37119eb83ac4211
Djordje Arandjelovic <djordjearandjelovic993@gmail.com>
2024-02-05 21:58:56 +0100

    initial commit

---
+++ FruitNutrition/FruitNutritionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitNutritionApp.swift
+//  FruitNutrition
+//
+//  Created by Djordje Arandjelovic on 5.2.24..
+//
+
+import SwiftUI
+
+@main
+struct FruitNutritionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
