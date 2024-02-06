9947f27cf5241f663b525165b48651ba9f6dcc83
Djordje Arandjelovic <djordjearandjelovic993@gmail.com>
2024-02-06 23:03:02 +0100

    Fruit Card

---
+++ FruitNutrition/View/StartButtonView.swift
@@ -0,0 +1,23 @@
+//
+//  StartButtonView.swift
+//  FruitNutrition
+//
+//  Created by Djordje Arandjelovic on 6.2.24..
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    //MARK: - PROPERTIES
+    
+    //MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+//MARK: - PREVIEW
+#Preview {
+    StartButtonView()
+        .previewLayout(.sizeThatFits)
+}
